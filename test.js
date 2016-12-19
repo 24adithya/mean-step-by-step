0bd29011aded3053aebc99eb8253b27e6eebb6e6
Adithya Narayan <Aditya.Narayana@fisglobal.com>
2016-12-19 15:40:03 +0530

    Eclipse preferences..pushing in this repo as i didn't want to make a separate repo just for the preferences :)

--- test.js
+++ test.js
@@ -4,3 +4,14 @@ console.log(m);
 console.log(m.add(3, 5));
 console.log(m.multiply(4, 5));
 console.log(m.factorial(4));
+
+console.log('Currently executing file is ' + __filename);
+console.log('It is located in ' + __dirname);
+
+console.log('Starting in ' + process.cwd());
+try {
+process.chdir('/');
+} catch (error) {
+console.error('chdir: ' + error.message);
+}
+console.log('Current working directory is now ' + process.cwd());
